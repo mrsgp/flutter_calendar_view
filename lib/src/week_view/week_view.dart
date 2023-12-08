cf39848f79a87201ce4afc8ada014457944826fc
Suman Pradhan <sumangopal@live.com>
2023-12-08 17:53:52 -0500

    remove use of Event.Title

--- lib/src/week_view/week_view.dart
+++ lib/src/week_view/week_view.dart
@@ -947,7 +947,7 @@ class WeekViewState<T extends Object?> extends State<WeekView<T>> {
     if (events.isNotEmpty)
       return RoundedEventTile(
         borderRadius: BorderRadius.circular(6.0),
-        title: '', //events[0].title,
+        title: events[0].title,
         titleStyle: events[0].titleStyle ??
             TextStyle(
               fontSize: 12,
